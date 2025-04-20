0c707c7459d388789bb9e470c4a8b904cf92cb79
Brianlian <brianlian@ad-ins.com>
2025-04-20 20:25:33 +0700

    set env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  BeUrl: "http://localhost:5033",
+  BeLocal: "http://localhost/myapp1",
+  BeLocal2: "https://localhost:7033",
+  BeIis: "http://localhost/myapp1"
 };
